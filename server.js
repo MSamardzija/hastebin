1c8e84b8219ab10b77621c62921c8f169a5caae3
Marko Samardzija <marko1716@its.edu.rs>
2023-06-24 23:52:21 +0200

    Adding New + Styling

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   const code = `Welcome to WasteBin
@@ -9,6 +10,10 @@ Use the commands in the top right cornder
 to create a new fiel to share with others.`
 
   res.render(`code-display.ejs`, { code })
+})
+
+app.get('/new', (req, res) => {
+  res.render('new')
 })
 
 app.listen(3000)
